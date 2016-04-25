55145b8eaff282a0b28b868c6dbf37c465a2b9a2
Abhishek Swain <swain@wustl.edu>
2016-04-25 18:26:56 -0500

    labwork

--- js/script.js
+++ js/script.js
@@ -1,12 +1,39 @@
-// loop
-var count = 0;
-var list = '';
+function getAnswers(){
+	var correctAnswers = 0;
+	var myQA = [
+		["Who is the Business School named after? (Last Name Only)"    ,    "OLIN"     ],
+		["Who is the Art School named after? (Last Name Only)"         ,    "FOX"      ],
+		["Who is the Social Work School named after? (Last Name Only)" ,    "BROWN"    ],
+		["Who is the Main Library named after? (Last Name Only)"       ,    "OLIN"     ],
+		["Who is the Main Quadrangle named after? (Last Name Only)"    ,    "BROOKINGS"]
+	];
+	
+	var questionDocument = "<h1> Results </h1>"; 
+	var i =0;
+	var ans = "";
+	
+	while(i < myQA.length){
+		
+		ans = prompt(myQA[i][0]);  
+		
+		if(ans.toUpperCase() == myQA[i][1]) { 
+		questionDocument += "<p>" + myQA[i][0] + ". You got the correct answer : " + myQA[i][1] + "</p>"; //user got it right!
+		correctAnswers++;
+		} else {
+		questionDocument += "<p>" + myQA[i][0] + ". You got the wrong answer : " + ans + ". The correct answer is " + myQA[i][1] + "</p>"; //user got it wrong!
+		}
+		
+		i++;
+	}
+	
+	questionDocument += "<h2> Your SCORE :" + correctAnswers + "/" + myQA.length + "</h2>";
+	return 	questionDocument; //this is the html of the results
+} 
 
-while (count < 10) {
-  list += '<li>current count is: ' + count + '</li>';
-  count += 1;
+function writeHTML(htmlString){
+	document.write(htmlString);
 }
 
-// output results
-document.write("<h1>Loops</h1>");
-document.write("<ul>" + list + "</ul>");
+var htmlResults = getAnswers(); 
+writeHTML(htmlResults);
+
